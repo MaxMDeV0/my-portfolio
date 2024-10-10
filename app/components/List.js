c7a99517c8b5d65c883303771957b6d164b41068
MaxMDeV0 <merter.maxence@gmail.com>
2024-10-11 01:26:40 +0200

    wip backoffice

--- app/components/List.js
+++ app/components/List.js
@@ -1,11 +1,38 @@
+"use client"
+
+import { useState, useEffect, useContext} from "react";
+import { SessionContext } from '@/app/context/SessionProvider';
+
 export default function List(props) {
-    const {isBlack, title, Item, ulClassName, data} = props;
+    const {isBlack, title, Item, ulClassName, apiUri} = props;
+    const [dataList, setDataList] = useState([]);
+    const session = useContext(SessionContext);
+    const [isEditing, setIsEditing] = useState(false);
+
+
+    useEffect(()=>{
+        fetch(`/api/${apiUri}`)
+        .then(response => response.json())
+        .then(json=>{setDataList(json[apiUri])})
+        .catch((err)=>{
+            console.log(err);
+        })
+    },[isEditing])
 
     return (
+        
         <section className={`flex flex-col py-10 px-4 space-y-7 items-center bg-${isBlack ? "black" : "white" } lg:py-[60px] min-[450px]:px-6 sm:px-10 md:px-16 lg:px-20 xl:px-24 2xl:px-28`}>
             <h2 className={`h-[4.5rem] text-center leading-[4.5rem] text-[1.75rem] text-${!isBlack ? "black" : "white" } lg:text-4xl lg:leading-[5.25rem] lg:h-[5.25rem] xl:text-5xl xl:leading-[6rem] xl:h-[6rem]`}>My <strong>{title}</strong></h2>
-            <ul className={ulClassName} >{data.map((item, index)=>{ return <Item item={item} key={index} index={index}/> } )}</ul>
+            <ul className={ulClassName} >{dataList.map((item, index)=>{ return <Item item={item} key={index} index={index}/> } )}
+                {isEditing && <Item item={isEditing} key={dataList.length+1} index={dataList.length+1} isEditing={isEditing} setIsEditing={setIsEditing}/>}
+            </ul>
+
+            {!!session && 
+                <button onClick={()=>setIsEditing(true)} className={`h-16 w-16 p-4 rounded-full border-2 border-${!isBlack ? "black" : "white" }`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill={`${!isBlack ? "black" : "white" }`}>
+                        <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" />
+                    </svg >
+                </button>}
         </section>
     )
-
 }
