9210dac97af11ea2092b72b74b96f69c230e2740
MaxMDeV0 <merter.maxence@gmail.com>
2024-10-15 15:36:39 +0200

    fix mediaSelector

--- app/components/MediaModal.js
+++ app/components/MediaModal.js
@@ -19,7 +19,8 @@ export default function MediaModal({setOpenModal, setPath}){
         })
     },[])
 
-    const clickHandler = () => {
+    const clickHandler = (e) => {
+        e.preventDefault()
         const fileInput = document.getElementById('file_input')
         fileInput.click()
     }
@@ -68,7 +69,7 @@ export default function MediaModal({setOpenModal, setPath}){
                         <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
                     </svg>
                 </button>
-                <button className="absolute bottom-5 right-5 flex bg-[#007bff] text-white rounded p-2 w-32 justify-between items-center" onClick={clickHandler}>
+                <button className="absolute bottom-5 right-5 flex bg-[#007bff] text-white rounded p-2 w-32 justify-between items-center" onClick={(e)=>clickHandler}>
                     <svg height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill={`white`}>
                         <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" />
                     </svg >
