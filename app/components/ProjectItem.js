c523e0ce0aaddfab90e643ad1ddf6c700617f271
MaxM <119800989+MaxMDeV0@users.noreply.github.com>
2025-05-27 09:46:00 +0700

    responsive img

--- app/components/ProjectItem.js
+++ app/components/ProjectItem.js
@@ -97,8 +97,8 @@ export default function ProjectItem({ item, index, CreatingHook, setDataList, is
                     setIsEditing={setIsEditing}
                 />
             }
-            <div className={`lg:flex space-y-7 ${index%2 == 1 ? "lg:flex-row-reverse" : ""} lg:space-y-[unset]`}>
-                <img src={item.path} alt="" className={`rounded w-full max-w-[610px] lg:w-6/12 lg:flex-1 ${index%2 == 1 ? "lg:pl-[2%]" : "lg:pr-[2%]"} `}/>
+            <div className={`items-center lg:flex space-y-7 ${index%2 == 1 ? "lg:flex-row-reverse" : ""} lg:space-y-[unset]`}>
+                <img src={item.path} alt="" className={`h-fit rounded w-full max-w-[610px] lg:w-6/12 lg:flex-1 ${index%2 == 1 ? "lg:pl-[2%]" : "lg:pr-[2%]"} `}/>
                 <div className={`flex-1 space-y-7  ${index%2 == 1 ? "lg:pr-[8%]" : "lg:pl-[8%]"} flex flex-col justify-center`}>
                     <h3 className="text-xl text-white font-extrabold">{index + 1 }</h3>
                     <h3 className="text-xl text-white font-extrabold">{item.title}</h3>
