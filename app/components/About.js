1ef013e7d1b0dece8d5ad5b083bf2019352535d1
MaxM <119800989+MaxMDeV0@users.noreply.github.com>
2025-05-27 16:34:55 +0700

    Update About.js

--- app/components/About.js
+++ app/components/About.js
@@ -10,8 +10,7 @@ export default function About() {
                     <div className="space-y-5 text-zinc-500 tracking-wide ">
                         <p>I&#39;m a motivated and curious full stack web developer with a strong foundation in both front-end and back-end technologies. I specialize in crafting user-friendly digital experiences and writing clean, maintainable, and efficient code. With a background in PHP and JavaScript, and hands-on experience using technologies like React, Node.js, and WordPress, I enjoy bringing design and functionality together to create seamless web applications.</p>
                         <p>My journey into web development began with a strong interest in science and technology. I completed a full-stack developer training program at 3W Academy. Since then, I&#39;ve grown through professional experiences like my role at Alvaria where I designed mockups, integrated responsive HTML/CSS, customized WordPress themes and plugins, and contributed to robust, tested solutions.</p>
-                        <p>Outside of my formal work,I&#39;built personal projects, including a CRM app using React Native and Firebase, and continuously push myself to learn new technologies. I thrive in both team-based and independent environments, and I bring a strong sense of service, organization, and adaptability to every project.</p>
-                        <p>Feel free to check out my GitHub to see what I&#39;m building.</p>
+                        <p>I&#39;ve built personal projects, including a CRM app using React Native and Firebase, and continuously push myself to learn new technologies. I thrive in both team-based and independent environments, and I bring a strong sense of service, organization, and adaptability to every project.</p>
                     </div>
                 </div>
             </div>
